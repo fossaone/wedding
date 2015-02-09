416474dba2294d6193aed6a0befd0fc3efe6953c
Nick <fossaone@gmail.com>
2015-02-08 23:01:09 -0800

    added to accomodations

--- wedding_website_project/settings.py
+++ wedding_website_project/settings.py
@@ -35,7 +35,7 @@ STATICFILES_DIRS = (
 SECRET_KEY = 'p*h-#1=c-2o&#_-wqp&h3!7q*ts2n#np36fo1mgigpq1c)l$fz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
 TEMPLATE_DEBUG = True
 
